8bc7b7e7b1c6c24a87e1d4583cb240798f2fba34
Marco Kühbauch <marcokuehbauch@gmail.com>
2019-07-24 09:41:41 +0200

    build basic structure to make graphql query inside react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [path, setPath] = useState(
+    "the-road-to-learn-react/the-road-to-learn-react"
+  );
+
+  useEffect(() => {
+    onFetchFromGithub();
+  });
+
+  const onChange = event => {};
+
+  const onSubmit = event => {
+    // fetch data
+
+    event.preventDefault();
+  };
+
+  const axiosGitHubGraphQL = axios.create({
+    baseURL: "https://api.github.com/graphql",
+    headers: {
+      Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+    }
+  });
+
+  const GET_ORGANIZATION = `
+    {
+      organization(login: "the-road-to-learn-react") {
+        name
+        url
+      }
+    }
+  `;
+
+  const onFetchFromGithub = () => {
+    axiosGitHubGraphQL
+      .post("", { query: GET_ORGANIZATION })
+      .then(result => console.log(result));
+  };
+
+  const TITLE = "React GraphQL GitbHub Client";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{TITLE}</h1>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">Show open issues for https://github.com</label>
+        <input
+          id="url"
+          type="text"
+          value={path}
+          onChange={onChange}
+          style={{ width: "300px" }}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <hr />
+
+      {/* Here comes the result */}
     </div>
   );
-}
+};
 
 export default App;
